86dfa34873fb601df1636c576c3e5d3591cce56d
sid <siddharth.sharma@leptonmaps.com>
2022-09-06 16:23:12 +0530

    initial

---
+++ tracking/src/main/java/com/lepton/tracking/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lepton.tracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startService();
+    }
+
+    public void startService(){
+        Log.i("startService","startService called");
+    }
+}
